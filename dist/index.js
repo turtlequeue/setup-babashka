e3b0e660d0935ef04246472a4a0894974302e2d5
nha <nha@users.noreply.github.com>
2021-03-29 10:55:45 +0100

    dist

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 module.exports =
 /******/ (() => { // webpackBootstrap
-/******/  var __webpack_modules__ = ({
+/******/ 	var __webpack_modules__ = ({
 
 /***/ 351:
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
@@ -5363,7 +5363,7 @@ function getBabashka(version) {
             yield io.mkdirP(tmpPath);
             core.info('temporary directory ' + tmpPath);
             const installerFile = yield tc.downloadTool("https://raw.githubusercontent.com/babashka/babashka/master/install");
-            core.info('INSTALLER FILE ' + installerFile);
+            core.info(`Downloaded installer file ${installerFile}`);
             yield exec.exec('bash', [installerFile, "--dir", tmpPath, "--version", version]);
             core.info(`babashka installed to ${tmpPath}`);
             toolPath = yield tc.cacheDir(tmpPath, 'Babashka', version, os.arch());
@@ -5542,84 +5542,84 @@ module.exports = require("util");
 
 /***/ })
 
-/******/  });
+/******/ 	});
 /************************************************************************/
-/******/  // The module cache
-/******/  var __webpack_module_cache__ = {};
-/******/
-/******/  // The require function
-/******/  function __webpack_require__(moduleId) {
-/******/    // Check if module is in cache
-/******/    if(__webpack_module_cache__[moduleId]) {
-/******/      return __webpack_module_cache__[moduleId].exports;
-/******/    }
-/******/    // Create a new module (and put it into the cache)
-/******/    var module = __webpack_module_cache__[moduleId] = {
-/******/      // no module.id needed
-/******/      // no module.loaded needed
-/******/      exports: {}
-/******/    };
-/******/
-/******/    // Execute the module function
-/******/    var threw = true;
-/******/    try {
-/******/      __webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/      threw = false;
-/******/    } finally {
-/******/      if(threw) delete __webpack_module_cache__[moduleId];
-/******/    }
-/******/
-/******/    // Return the exports of the module
-/******/    return module.exports;
-/******/  }
-/******/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
 /************************************************************************/
-/******/  /* webpack/runtime/compat get default export */
-/******/  (() => {
-/******/    // getDefaultExport function for compatibility with non-harmony modules
-/******/    __webpack_require__.n = (module) => {
-/******/      var getter = module && module.__esModule ?
-/******/        () => module['default'] :
-/******/        () => module;
-/******/      __webpack_require__.d(getter, { a: getter });
-/******/      return getter;
-/******/    };
-/******/  })();
-/******/
-/******/  /* webpack/runtime/define property getters */
-/******/  (() => {
-/******/    // define getter functions for harmony exports
-/******/    __webpack_require__.d = (exports, definition) => {
-/******/      for(var key in definition) {
-/******/        if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/          Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/        }
-/******/      }
-/******/    };
-/******/  })();
-/******/
-/******/  /* webpack/runtime/hasOwnProperty shorthand */
-/******/  (() => {
-/******/    __webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
-/******/  })();
-/******/
-/******/  /* webpack/runtime/make namespace object */
-/******/  (() => {
-/******/    // define __esModule on exports
-/******/    __webpack_require__.r = (exports) => {
-/******/      if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/        Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/      }
-/******/      Object.defineProperty(exports, '__esModule', { value: true });
-/******/    };
-/******/  })();
-/******/
-/******/  /* webpack/runtime/compat */
-/******/
-/******/  __webpack_require__.ab = __dirname + "/";/************************************************************************/
-/******/  // module exports must be returned from runtime so entry inlining is disabled
-/******/  // startup
-/******/  // Load entry module and return exports
-/******/  return __webpack_require__(88);
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => module['default'] :
+/******/ 				() => module;
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	__webpack_require__.ab = __dirname + "/";/************************************************************************/
+/******/ 	// module exports must be returned from runtime so entry inlining is disabled
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(88);
 /******/ })()
 ;
